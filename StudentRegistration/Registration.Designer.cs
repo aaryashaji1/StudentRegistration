d42e47b9dbd26f0875de85204e9d753a388856e8
aaryashaji1 <aaryashajit@gmail.com>
2021-04-09 05:19:49 -0500

    Add project files.

---
+++ StudentRegistration/Registration.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace StudentRegistration
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.checkPg = new System.Windows.Forms.CheckBox();
+            this.checkUg = new System.Windows.Forms.CheckBox();
+            this.checkPlusTwo = new System.Windows.Forms.CheckBox();
+            this.checkTen = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(73, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Qualification";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ProfilePicture";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.txtName.Location = new System.Drawing.Point(218, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.txtAddress.Location = new System.Drawing.Point(218, 92);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(149, 33);
+            this.txtAddress.TabIndex = 6;
+            this.txtAddress.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(218, 151);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(48, 17);
+            this.radioMale.TabIndex = 7;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(308, 151);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioFemale.TabIndex = 8;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // checkPg
+            // 
+            this.checkPg.AutoSize = true;
+            this.checkPg.Location = new System.Drawing.Point(218, 197);
+            this.checkPg.Name = "checkPg";
+            this.checkPg.Size = new System.Drawing.Size(41, 17);
+            this.checkPg.TabIndex = 9;
+            this.checkPg.Text = "PG";
+            this.checkPg.UseVisualStyleBackColor = true;
+            // 
+            // checkUg
+            // 
+            this.checkUg.AutoSize = true;
+            this.checkUg.Location = new System.Drawing.Point(313, 197);
+            this.checkUg.Name = "checkUg";
+            this.checkUg.Size = new System.Drawing.Size(42, 17);
+            this.checkUg.TabIndex = 10;
+            this.checkUg.Text = "UG";
+            this.checkUg.UseVisualStyleBackColor = true;
+            // 
+            // checkPlusTwo
+            // 
+            this.checkPlusTwo.AutoSize = true;
+            this.checkPlusTwo.Location = new System.Drawing.Point(218, 238);
+            this.checkPlusTwo.Name = "checkPlusTwo";
+            this.checkPlusTwo.Size = new System.Drawing.Size(70, 17);
+            this.checkPlusTwo.TabIndex = 11;
+            this.checkPlusTwo.Text = "Plus Two";
+            this.checkPlusTwo.UseVisualStyleBackColor = true;
+            // 
+            // checkTen
+            // 
+            this.checkTen.AutoSize = true;
+            this.checkTen.Location = new System.Drawing.Point(313, 238);
+            this.checkTen.Name = "checkTen";
+            this.checkTen.Size = new System.Drawing.Size(53, 17);
+            this.checkTen.TabIndex = 12;
+            this.checkTen.Text = "SSLC";
+            this.checkTen.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(218, 292);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnBrowse.Location = new System.Drawing.Point(316, 404);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(51, 23);
+            this.btnBrowse.TabIndex = 14;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSave.Location = new System.Drawing.Point(213, 443);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 35);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnRead.Location = new System.Drawing.Point(294, 443);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 35);
+            this.btnRead.TabIndex = 16;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(498, 503);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkTen);
+            this.Controls.Add(this.checkPlusTwo);
+            this.Controls.Add(this.checkUg);
+            this.Controls.Add(this.checkPg);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.CheckBox checkPg;
+        private System.Windows.Forms.CheckBox checkUg;
+        private System.Windows.Forms.CheckBox checkPlusTwo;
+        private System.Windows.Forms.CheckBox checkTen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ StudentRegistration/Registration.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistration
+{
+    public partial class Registration : Form
+    {
+        String connectionString = "Server=ARYASHAJI\\SQLEXPRESS;Database=StudentRegistrationDatabase;Trusted_Connection=True;";
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK) ;
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+
+            }
+            string path = openFileDialog.FileName;
+            File.WriteAllText(@"Downloads", path);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string Address = txtAddress.Text;
+            
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("StudentSave", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Name", txtName.Text);
+            command.Parameters.AddWithValue("Address", txtAddress.Text);
+            if (radioMale.Checked == true)
+            {
+                command.Parameters.AddWithValue("Gender", "M");
+
+            }
+            if (radioFemale.Checked == true)
+            {
+                command.Parameters.AddWithValue("Gender", "F");
+            }
+
+            command.Parameters.AddWithValue("Sslc", checkTen.Checked);
+            command.Parameters.AddWithValue("PlusTwo", checkPlusTwo.Checked);
+            command.Parameters.AddWithValue("Ug", checkUg.Checked);
+            command.Parameters.AddWithValue("Pg", checkPg.Checked);
+            command.Parameters.AddWithValue("Image", File.ReadAllText(@"Downloads"));
+            command.ExecuteNonQuery();
+            connection.Close();
+            txtName.Text = txtAddress.Text = "";
+            checkTen.Checked = checkPlusTwo.Checked = checkUg.Checked = checkPg.Checked = false;
+            radioFemale.Checked = radioMale.Checked = false;
+            pictureBox1.Image = null;
+            MessageBox.Show("Saved");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+
+                SqlCommand command = new SqlCommand("StudentRead", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("Name", txtName.Text);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    
+                    txtAddress.Text = reader["Address"].ToString();
+                    if (reader["Gender"].ToString() == "M")
+                    {
+                        radioMale.Checked = true;
+                    }
+                    if (reader["Gender"].ToString() == "F")
+                    {
+                        radioFemale.Checked = true;
+                    }
+                    if (reader["Sslc"].ToString() == "1")
+                    {
+                        checkTen.Checked = true;
+
+                    }
+                    if (reader["PlusTwo"].ToString() == "1")
+                    {
+                        checkPlusTwo.Checked = true;
+                    }
+                    if (reader["Ug"].ToString() == "1")
+                    {
+                        checkUg.Checked = true;
+                    }
+                    if (reader["Pg"].ToString() == "1")
+                    {
+                        checkPg.Checked = true;
+                    }
+                    pictureBox1.Image = Image.FromFile("" + reader["Image"].ToString());
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+    }
+
+
+    
+
